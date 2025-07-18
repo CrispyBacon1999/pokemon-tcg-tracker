1c71fdd67990a58cd971d3b1337d966e020be1f2
CrispyBacon1999 <bacon.josh09@gmail.com>
2025-07-17 23:19:56 -0400

    Add sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,12 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://6940683bf8ce72761d6bdb99fedc8480@o4508147244335104.ingest.us.sentry.io/4509687379918848",
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
---
+++ src/instrumentation-client.ts
@@ -0,0 +1,13 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://6940683bf8ce72761d6bdb99fedc8480@o4508147244335104.ingest.us.sentry.io/4509687379918848",
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
